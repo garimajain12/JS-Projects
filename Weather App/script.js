6d20c8c8559181fc18c88378b6bdd240bd801693
Garima <garimajain120403@gmail.com>
2025-03-09 13:02:57 +0530

    Initial commit: Added Todo List and Weather App projects

---
+++ Weather App/script.js
@@ -0,0 +1,52 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const cityInput = document.getElementById("city-input");
+  const getWeatherButton = document.getElementById("get-weather-btn");
+  const weatherInfo = document.getElementById("weather-info");
+  const cityNameDisplay = document.getElementById("city-name");
+  const temperatureDisplay = document.getElementById("temperature");
+  const descriptionDisplay = document.getElementById("description");
+  const errorMessage = document.getElementById("error-message");
+
+  const API_KEY = "aa5550a7acb647075070c0cc936ebec3";
+
+  getWeatherButton.addEventListener("click", async () => {
+    const city = cityInput.value.trim();
+    if (!city) return;
+
+    try {
+      const weatherData = await fetchWeatherData(city);
+      displayWeatherData(weatherData);
+    } catch (error) {
+      showError();
+    }
+  });
+
+  async function fetchWeatherData(city) {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error("City not found");
+    }
+    const data = await response.json();
+    return data;
+  }
+
+  function displayWeatherData(data) {
+    const { name, main, weather } = data;
+    cityNameDisplay.textContent = name;
+    temperatureDisplay.textContent = `Temperature : ${main.temp}`;
+    descriptionDisplay.textContent = `Weather : ${weather[0].description}`;
+
+    // unlock the Display
+    weatherInfo.classList.remove("hidden");
+    errorMessage.classList.add("hidden");
+  }
+
+  function showError(message = "City not found. Please try again.") {
+    weatherInfo.classList.add("hidden");
+    errorMessage.textContent = message;
+    errorMessage.classList.remove("hidden");
+  }
+});
